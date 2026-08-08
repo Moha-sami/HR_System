a1cfd5c19c75f2a7369483f72d9f72e60c1c587b
Ahmed Gamal <ahmedgmal010909@gmail.com>
2026-08-08 12:35:55 -0700

    SCRUM-101-Create-EmployeeDocument-Interface

---
+++ src/Buy2.Domain/Entities/EmployeeDocument.cs
@@ -0,0 +1,9 @@
+﻿namespace Buy2.Domain.Entities;
+
+public class EmployeeDocument : BaseEntity
+{
+    public int EmployeeId { get; set; }
+    public string Category { get; set; } = string.Empty;
+    public string StorageUrl { get; set; } = string.Empty;
+    public virtual Employee Employee { get; set; } = null!;
+}
