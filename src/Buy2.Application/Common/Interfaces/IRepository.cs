baed47aafb3bfdf5dbb1c3299e4fafe67b108143
Ahmed Gamal <ahmedgmal010909@gmail.com>
2026-08-08 05:39:33 -0700

    SCRUM-18-Create-IRepository-Interface

--- src/Buy2.Application/Common/Interfaces/IRepository.cs
+++ src/Buy2.Application/Common/Interfaces/IRepository.cs
@@ -4,9 +4,9 @@ namespace Buy2.Application.Common.Interfaces;
 
 public interface IRepository<T> where T: BaseEntity
 {
-    public Task<T?> GetByIdAsync(int id);
-    public Task<IEnumerable<T>> GetAllAsync();
-    public Task AddAsync(T entity);
-    public void Update(T entity);
-    public void Delete(T entity);
+    Task<T?> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
 }
