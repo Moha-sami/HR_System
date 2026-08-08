0dda5e8aa86c525203e5852575c4bc75fc730966
Ahmed Gamal <ahmedgmal010909@gmail.com>
2026-08-08 12:46:14 -0700

    SCRUM-103-Create-PointsTransaction-Entity

---
+++ src/Buy2.Domain/Entities/PointsTransaction.cs
@@ -0,0 +1,10 @@
+﻿namespace Buy2.Domain.Entities;
+public class PointsTransaction : BaseEntity
+{
+    public int EmployeeId { get; set; }
+    public int? PointsRuleId { get; set; }
+    public decimal Amount { get; set; }
+    public string TransactionType { get; set; } = string.Empty;
+    public virtual Employee Employee { get; set; } = null!;
+    public virtual PointsRule? PointsRule { get; set; }
+}
