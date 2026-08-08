3159e15634e603c89e5dc2b6c3213590cc6c0dcc
Moha-sami <engmohasami@gmail.com>
2026-08-08 20:50:57 +0300

    SCRUM-24  Task 19: Create Site DTO Records

---
+++ src/Buy2.Application/DTOs/CreateSiteDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Buy2.Application.DTOs;
+
+public record CreateSiteDto
+{
+    public string SiteName { get; init; } = string.Empty;
+    public double Latitude { get; init; }
+    public double Longitude { get; init; }
+    public List<string> MacWhitelist { get; init; } = new List<string>();
+}
