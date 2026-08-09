20c1680db3d0589dcd4fbda3d70fdfa1b767f850
Ahmed Gamal <ahmedgmal010909@gmail.com>
2026-08-09 08:25:10 -0700

    SCRUM-117-Create-DisciplinaryViolationConfiguration-Infrastructure

---
+++ src/Buy2.Infrastructure/Persistence/Configurations/DisciplinaryViolationConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Buy2.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Buy2.Infrastructure.Persistence.Configurations;
+
+public class DisciplinaryViolationConfiguration : IEntityTypeConfiguration<DisciplinaryViolation>
+{
+    public void Configure(EntityTypeBuilder<DisciplinaryViolation> builder)
+    {
+        builder.Property(d => d.Severity)
+            .IsRequired()
+            .HasColumnType("varchar(20)");
+        builder.Property(d => d.Description)
+            .IsRequired()
+            .HasColumnType("nvarchar(1000)");
+
+        builder.HasOne<Employee>()
+            .WithMany()
+            .HasForeignKey(d => d.EmployeeId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
