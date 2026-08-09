7e0a3ccb88187fe154634480538f1a35708da03a
Mahmoud <mahmoudolaim682@gmail.com>
2026-08-10 00:10:28 +0300

    SCRUM-116 Change delete behavior to cascade for Employee relation

--- src/Buy2.Infrastructure/Persistence/Configurations/EmployeeDocumentConfiguration.cs
+++ src/Buy2.Infrastructure/Persistence/Configurations/EmployeeDocumentConfiguration.cs
@@ -19,7 +19,7 @@ namespace Buy2.Infrastructure.Persistence.Configurations
             builder.HasOne(ed => ed.Employee)
                 .WithMany()
                 .HasForeignKey(ed => ed.EmployeeId)
-                .OnDelete(DeleteBehavior.Restrict);
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
