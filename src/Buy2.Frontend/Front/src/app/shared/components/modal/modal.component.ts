c7052a1e4c31f057ad2f9d0bad9e684a4284e72c
mohamed shalapy <mohamedshalapy7919@gmail.com>
2026-08-09 16:53:32 +0300

    SCRUM-58: Rename modal component

--- src/Buy2.Frontend/Front/src/app/shared/components/modal/modal.component.ts
+++ src/Buy2.Frontend/Front/src/app/shared/components/modal/modal.component.ts
@@ -3,10 +3,10 @@ import { Component, input, output} from '@angular/core';
 @Component({
   selector: 'app-modal',
   imports: [],
-  templateUrl: './modal.html',
-  styleUrl: './modal.css',
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css',
 })
-export class Modal {
+export class ModalComponent {
   constructor(){}
 
   size = input<'small' | 'medium' | 'large'>('medium');
