1801628c2d92a4028fea7d01c29fff8b5d1a0b56
Ahmed Gamal <ahmedgmal010909@gmail.com>
2026-08-10 09:03:29 -0700

    SCRUM-189-Create-Employee-Onboarding-Endpoint

---
+++ src/Buy2.Api/Controllers/EmployeeOnboardingController.cs
@@ -0,0 +1,24 @@
+﻿using Buy2.Application.Features.Employees.OnboardEmployee;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buy2.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/employees/onboard")]
+public class EmployeeOnboardingController : ControllerBase
+{
+    private readonly ISender _mediator;
+    public EmployeeOnboardingController(ISender mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<int>> Onboard(OnboardEmployeeCommand command)
+    {
+        var employeeId = await _mediator.Send(command);
+
+        return Ok(employeeId);
+    }
+}
---
+++ src/Buy2.Application/Features/Employees/OnboardEmployee/OnboardEmployeeCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Buy2.Application.Features.Employees.OnboardEmployee;
+public record OnboardEmployeeCommand(
+        string FirstName,
+        string LastName,
+        string Email,
+        int JobRoleId,
+        int SiteId
+    ) : IRequest<int>;
