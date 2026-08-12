dc5a7b25cb25e74cb477cb9dfa54a84fb5e0fb4c
Ahmed Gamal <ahmedgmal010909@gmail.com>
2026-08-12 10:14:51 -0700

    SCRUM-196-Create-Claim-Shift-Endpoint

---
+++ src/Buy2.Api/Controllers/ShiftClaimsController.cs
@@ -0,0 +1,29 @@
+﻿using Buy2.Application.Features.ShiftMarket.ClaimShift;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buy2.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/shift-market/claims/{id}")]
+public class ShiftClaimsController : ControllerBase
+{
+    private readonly ISender _mediator;
+    public ShiftClaimsController(ISender mediator)
+    {
+        _mediator = mediator;
+    }
+
+
+    [HttpPost]
+    public async Task<ActionResult<bool>> Create(int id, ClaimShiftCommand command)
+    {
+        if(command.ShiftId != id)
+        {
+            return BadRequest("Shift Not Match");
+        }
+        var result = await _mediator.Send(command);
+
+        return Ok(result);
+    }
+}
---
+++ src/Buy2.Application/Features/ShiftMarket/ClaimShift/ClaimShiftCommand.cs
@@ -0,0 +1,59 @@
+﻿using Buy2.Application.Common.Interfaces;
+using Buy2.Domain.Entities;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace Buy2.Application.Features.ShiftMarket.ClaimShift;
+
+
+public record ClaimShiftCommand
+    (
+        int ShiftId, int EmployeeId, string OvertimeJustification
+    ) : IRequest<bool>;
+
+public class ClaimShiftCommandHandler : IRequestHandler<ClaimShiftCommand, bool>
+{
+    private readonly IRepository<ShiftClaim> _shiftClaimRepositroy;
+    private readonly IRepository<ShiftEntity> _shiftRepository;
+
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ClaimShiftCommandHandler(IRepository<ShiftClaim> shiftClaimRepository, IRepository<ShiftEntity> shiftEntityRepository, IUnitOfWork unitOfWork)
+    {
+        _shiftClaimRepositroy = shiftClaimRepository;
+        _shiftRepository = shiftEntityRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<bool> Handle(ClaimShiftCommand command, CancellationToken cancellationToken)
+    {
+        var shift = await _shiftRepository.GetByIdAsync(command.ShiftId);
+
+        if (shift is null)
+        {
+            throw new ValidationException($"Shift not found.");
+        }
+        if (!shift.IsPublished)
+        {
+            throw new ValidationException($"Shift is not published.");
+        }
+        if (shift.EmployeeId is not null)
+        {
+            throw new ValidationException($"Shift is already claimed.");
+        }
+
+        var shiftClam = new ShiftClaim
+        {
+            ShiftId = command.ShiftId,
+            EmployeeId = command.EmployeeId,
+            Status = "Pending",
+            OvertimeJustification = command.OvertimeJustification
+        };
+
+        await _shiftClaimRepositroy.AddAsync(shiftClam);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return true;
+    }
+
+}
