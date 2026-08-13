5eb627144d5b6b488ad558145ab46a4477204b39
mohamed shalapy <mohamedshalapy7919@gmail.com>
2026-08-13 03:24:49 +0300

    SCRUM-147: Fix conflic in app confige due to  SCRUM-144-i18n

--- src/Buy2.Frontend/Front/src/app/app.config.ts
+++ src/Buy2.Frontend/Front/src/app/app.config.ts
@@ -1,14 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { LanguageService } from './core/services/language.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
-  ],
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/assets/i18n/',
+        suffix: '.json',
+      }),
+      fallbackLang: 'en',
+      lang: 'en',
+    }),
+    provideAppInitializer(() => inject(LanguageService).initialize()),
+  ]
 };
