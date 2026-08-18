aa908aa81cf416404b2a203202dcedc005a4cdd7
Moha-sami <engmohasami@gmail.com>
2026-08-18 14:51:26 +0300

    SCRUM-214 Create PayrollProfile Entity

--- src/Buy2.Domain/Entities/PayrollProfile.cs
+++ src/Buy2.Domain/Entities/PayrollProfile.cs
@@ -4,6 +4,16 @@ namespace Buy2.Domain.Entities;
 
 public class PayrollProfile : BaseEntity
 {
-    public decimal BaseSalary { get; set; }
+    public int EmployeeId { get; set; }
     public SalaryType SalaryType { get; set; }
+    public string PayoutPeriod { get; set; } = string.Empty;
+    public int PayoutDay { get; set; }
+    public DayOfWeek WorkWeekStart { get; set; } = DayOfWeek.Sunday;
+    public DayOfWeek WorkWeekEnd { get; set; } = DayOfWeek.Thursday;
+    public decimal PaymentAmount { get; set; }
+    public decimal OvertimeThresholdHours { get; set; }
+    public decimal OvertimeHourlyRate { get; set; }
+
+    // Navigation Property (1:1 with Employee)
+    public Employee? Employee { get; set; }
 }
