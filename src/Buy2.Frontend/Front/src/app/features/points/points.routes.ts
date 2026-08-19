b709924da645943e6e18b4d5c81f0c40dac2742e
Mohamed Shalapy <mohamedshalapy7919@gmail.com>
2026-08-19 23:33:05 +0300

    Revert "SCRUM-211: Implement points automation feature"

--- src/Buy2.Frontend/Front/src/app/features/points/points.routes.ts
+++ src/Buy2.Frontend/Front/src/app/features/points/points.routes.ts
@@ -1,10 +1,6 @@
 import type { Routes } from '@angular/router';
-import { PointsAutomationComponent } from './components/points-automation/points-automation.component';
-import { PointsAutomationSetupComponent } from './components/points-automation-setup/points-automation-setup.component';
 import { PointsManagement } from './components/points-management';
 
 export const POINTS_ROUTES: Routes = [
   { path: '', component: PointsManagement },
-  { path: 'automation', component: PointsAutomationComponent },
-  { path: 'automation/setup', component: PointsAutomationSetupComponent },
 ];
