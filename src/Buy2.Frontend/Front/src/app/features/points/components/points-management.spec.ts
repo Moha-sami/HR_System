886f016f45bb61c24e78d8aa08c6cb34f6d4ce9b
tigo24 <mtiggo24@gmail.com>
2026-08-19 14:19:06 +0300

    SCRUM-150: refine the style to match the entire project

--- src/Buy2.Frontend/Front/src/app/features/points/components/points-management.spec.ts
+++ src/Buy2.Frontend/Front/src/app/features/points/components/points-management.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PointsManagement } from '../points-management';
+import { PointsManagement } from './points-management';
 
 describe('PointsManagement', () => {
   let component: PointsManagement;
