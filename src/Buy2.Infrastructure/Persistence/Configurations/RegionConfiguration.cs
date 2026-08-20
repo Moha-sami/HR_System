b3345b02a65ad2270275e28dc164f69bda6615d0
Ahmed Gamal <ahmedgmal010909@gmail.com>
2026-08-20 13:11:17 -0700

    SCRUM-227-SITE-ENTITY-1

---
+++ src/Buy2.Infrastructure/Persistence/Configurations/RegionConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Buy2.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Buy2.Infrastructure.Persistence.Configurations;
+public class RegionConfiguration : IEntityTypeConfiguration<Region>
+{
+    public void Configure(EntityTypeBuilder<Region> builder)
+    {
+        builder.Property(r => r.Name)
+          .IsRequired()
+          .HasColumnType("nvarchar(100)");
+        builder.HasIndex(r => r.Name)
+            .IsUnique();
+        builder.Property(r => r.Description)
+          .IsRequired(false)
+          .HasColumnType("nvarchar(500)");
+
+        builder.HasMany<Site>()
+            .WithOne()
+            .HasForeignKey(s => s.RegionId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
