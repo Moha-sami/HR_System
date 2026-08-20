21feb2377dfe75ae4d1c269fd3d9b99c99d43168
tigo24 <mtiggo24@gmail.com>
2026-08-20 13:50:10 +0300

    Fixing the routing of "add transaction"

--- src/Buy2.Frontend/Front/src/app/features/points/points.routes.ts
+++ src/Buy2.Frontend/Front/src/app/features/points/points.routes.ts
@@ -1,6 +1,8 @@
 import type { Routes } from '@angular/router';
 import { PointsManagement } from './components/points-management';
+import { AddTransactionComponent } from './components/add-transaction/add-transaction.component';
 
 export const POINTS_ROUTES: Routes = [
   { path: '', component: PointsManagement },
+  { path: 'add', component: AddTransactionComponent }
 ];
