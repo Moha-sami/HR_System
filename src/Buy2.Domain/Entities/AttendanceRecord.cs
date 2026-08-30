07be24c2d15ee4af66d135f6ebc08d9bef3af079
Mahmoud <mahmoudolaim682@gmail.com>
2026-08-30 03:15:24 +0300

    Revert "SCRUM-309 Add attendance tracking fields and status updates"
    This reverts commit 689a36a773100dd6b140ef3b80023541b2842ef6.

--- src/Buy2.Domain/Entities/AttendanceRecord.cs
+++ src/Buy2.Domain/Entities/AttendanceRecord.cs
@@ -10,8 +10,6 @@ public class AttendanceRecord : BaseEntity
     public DateTimeOffset? ClockOutTime { get; set; }
     public int? ScheduledShiftId { get; set; }
     public int? LatenessMinutes { get; set; }
-    public int? EarlyLeaveMinutes { get; set; }
-    public decimal ScheduledHours { get; set; }
     public decimal HoursWorked { get; set; }
     public decimal OvertimeHours { get; set; }
     public decimal BreakMinutes { get; set; }
--- src/Buy2.Domain/Enums/AttendanceDayStatus.cs
+++ src/Buy2.Domain/Enums/AttendanceDayStatus.cs
@@ -2,12 +2,12 @@ namespace Buy2.Domain.Enums;
 
 public enum AttendanceDayStatus
 {
-    Present = 1,
-    ApprovedLeave = 2,
-    UnapprovedLeave = 3,
-    PartialLeave = 4,
-    NoAttendance = 5,
-    PublicHoliday = 6,
-    AttendanceNotRequired = 7
+    OnTime = 1,
+    Late = 2,
+    ApprovedLeave = 3,
+    UnapprovedLeave = 4,
+    PartialLeave = 5,
+    NoAttendance = 6,
+    PublicHoliday = 7,
+    AttendanceNotRequired = 8
 }
-
