33f79cfdb6940818b7edb7009d1ba9b5c644d491
Ahmed Gamal <ahmedgmal010909@gmail.com>
2026-09-04 08:07:09 -0700

    SCRUM-316-REWARD-ENTITY-1

---
+++ src/Buy2.Domain/Entities/RewardVoucher.cs
@@ -0,0 +1,12 @@
+﻿using Buy2.Domain.Enums;
+
+namespace Buy2.Domain.Entities;
+
+public class RewardVoucher : BaseEntity
+{
+    public int BatchId { get; set; }
+    public string Code { get; set; } = string.Empty;
+    public VoucherStatus Status { get; set; }
+    public int RewardItemId { get; set; }
+    public RewardItem RewardItem { get; set; } = null!;
+}
---
+++ src/Buy2.Domain/Enums/VoucherStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace Buy2.Domain.Enums;
+
+public enum VoucherStatus
+{
+    Available = 1,
+    Redeemed = 2
+}
