37b09ae9705610cbc8ae21b18489786d5997dbf6
mohamed shalapy <mohamedshalapy7919@gmail.com>
2026-09-08 20:46:31 +0300

    SCRUM-373-host-mock-api-on-glitch-instead-of-render

--- src/Buy2.Frontend/Front/src/environments/environment.prod.ts
+++ src/Buy2.Frontend/Front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  jsonServerUrl: 'https://hr-system-mock-api.onrender.com',
+  jsonServerUrl: 'https://hr-system-mock-api.glitch.me',
   baseUrl: 'https://hr-system-api.runasp.net/api/v1',
 };
