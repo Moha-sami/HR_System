196a3becae36827757348d5702719d773ecde646
Mohamed Shalapy <mohamedshalapy7919@gmail.com>
2026-09-08 20:59:49 +0300

    Revert "SCRUM-373: host json-server on Render for Vercel frontend"

--- src/Buy2.Frontend/Front/src/environments/environment.prod.ts
+++ src/Buy2.Frontend/Front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  jsonServerUrl: 'https://hr-system-mock-api.onrender.com',
+  jsonServerUrl: 'http://localhost:3000',
   baseUrl: 'https://hr-system-api.runasp.net/api/v1',
 };
